d0b667605c7b531a1c5e041e8d4d55741de92bc9
Benny Thai <bennythai2013@hotmail.com>
2018-03-31 19:56:10 -0400

    Can now do simple calculations with = btn

--- script.js
+++ script.js
@@ -24,34 +24,61 @@ var operate = function(a,b,operator){
 		return subtract(a,b);
 	}
 }
-/*
-var getInput = function(){
-	var input;
-	$('button').click(function(){
-		input = $(this).text();
-	});
-	return input;
-};
-*/
-var input,addInput;
-
+var input;
+var result=[];
+var beforeOP,afterOP,total;
 $(document).ready(function(){
 	$('.number').click(function(){
 		input = $(this).text();  //input is a string so parseInt turns it into an integer
+		result.push(input);
+		$('.answer').text(result);
 		//if(isNaN(parseInt(input)) == false){ //isNaN checks if it is not a number
-		
-			const number = input;
-			$('.answer').text(number);
-			console.log(number);
+		console.log(input);
+	});
+        $('.add').click(function(){
+		input = $(this).text();
+		console.log(input);
+		result.push(input);
+		$('.answer').text(result);	
+	});
+	$('.subtract').click(function(){
+		input = $(this).text();
+		result.push(input);
+		$('.answer').text(result);
+
+	});
+	$('.multiply').click(function(){
+		input = $(this).text();
+		result.push(input);
+		$('.answer').text(result);
 		
 	});
-        $('.add').click(function(){			
-			const addInput = $(this).text();
-			console.log("This is add " + addInput);
+	$('.divide').click(function(){
+		input = $(this).text();
+		result.push(input);
+		$('.answer').text(result.join(""));
+	})
+	$('.equal').click(function(){
+		result.forEach(function(element,index,array){
+			if(isNaN(parseInt(element)) == true){	
+				beforeOP = result.splice(0,index).join("");
+				afterOP = result.splice(1).join("");
+				if(element == "+"){
+					total = parseInt(beforeOP) + parseInt(afterOP);		
+				}
+				if(element == "-"){
+					total = parseInt(beforeOP) - parseInt(afterOP);
+				}
+				if(element == "*"){
+					total = parseInt(beforeOP) * parseInt(afterOP);
+				}
+				if(element == "/"){
+					total = parseInt(beforeOP) / parseInt(afterOP);
+				}
+				$('.answer').text(total);
+			}
 	
+
+		});
 	});
-	//if(input == "="){
-	//	$('.answer').text(add(input,addInput));
-	
-	//}
 });
